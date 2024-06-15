e010027af8feb6f015bd00b8b2f577c86af6a1a7
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-06-15 14:13:14 +0530

    add a way to test your code and then commit

--- code/git/autocommit.bash
+++ code/git/autocommit.bash
@@ -1,18 +1,39 @@
 # !/bin/bash
 
+
+
+# Running Tests
+echo "Running tests...."
+
+# test command that exit with 0 on passing
+echo "No Tests Found!!!"
+echo "For add them edit the script!!!!"
+
+if [ $? -eq 0 ]; then
+    echo "Passed all test cases!!!!"
+else
+    echo "Test Failed!!!"
+    exit 1
+fi
+
+echo ""
+
+
+
 # Staging Files
 echo "Staging Files Changes...."
 git add .
-
 echo ""
+
+
 
 # Commiting Change
 read -p "Enter a commit message: " message
-
 echo "Commiting the change..."
 git commit -m "$message" 
-
 echo ""
+
+
 
 # Pushing them to Remote
 read -p "Want to push it to remote (y/n): " res
--- code/git/autocommit.ps1
+++ code/git/autocommit.ps1
@@ -1,23 +1,40 @@
-# !/bin/bash
+# Running Tests
+Write-Host "Running tests...."
+
+# test command that exit with 0 on passing
+Write-Host "No Tests Found!!!!"
+Write-Host "For add them edit the script!!!!"
+
+if ( $LASTEXITCODE -eq 0 ) {
+    Write-Host "Passed all test cases!!!!"
+} else {
+    Write-Host "Test Failed!!!"
+    EXIT 1
+}
+
+Write-Host ""
+
+
 
 # Staging Files
 Write-Host "Staging Files Changes...."
 git add .
-
 Write-Host ""
+
+
 
 # Commiting Change
 $message =  Read-Host "Enter a commit message"
-
 Write-Host "Commiting the change..."
 git commit -m "$message" 
-
 Write-Host ""
+
+
 
 # Pushing them to Remote
 $res = Read-Host "Want to push it to remote (y/n)"
 
-if ($res -eq "y") {
+if ( $res -eq "y" ) {
     Write-Host "Pushing your change...."
     git push
 }
