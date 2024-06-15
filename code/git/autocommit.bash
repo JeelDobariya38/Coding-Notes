6033003ded05af600c7b9fbb26e8892a1f58fbab
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-06-15 13:49:12 +0530

    add scripts that help user in commiting there changes to git in one cmd

--- code/git/autocommit.bash
+++ code/git/autocommit.bash
@@ -1,1 +1,25 @@
-echo "Hello Coding Notes"
+# !/bin/bash
+
+# Staging Files
+echo "Staging Files Changes...."
+git add .
+
+echo ""
+
+# Commiting Change
+read -p "Enter a commit message: " message
+
+echo "Commiting the change..."
+git commit -m "$message" 
+
+echo ""
+
+# Pushing them to Remote
+read -p "Want to push it to remote (y/n): " res
+
+if [ $res -eq "y" ]; then
+    echo "Pushing your change...."
+    git push
+fi
+
+echo ""
--- code/git/autocommit.ps1
+++ code/git/autocommit.ps1
@@ -1,1 +1,25 @@
-echo "Hello Coding Notes"
+# !/bin/bash
+
+# Staging Files
+Write-Host "Staging Files Changes...."
+git add .
+
+Write-Host ""
+
+# Commiting Change
+$message =  Read-Host "Enter a commit message"
+
+Write-Host "Commiting the change..."
+git commit -m "$message" 
+
+Write-Host ""
+
+# Pushing them to Remote
+$res = Read-Host "Want to push it to remote (y/n)"
+
+if ($res -eq "y") {
+    Write-Host "Pushing your change...."
+    git push
+}
+
+Write-Host ""
